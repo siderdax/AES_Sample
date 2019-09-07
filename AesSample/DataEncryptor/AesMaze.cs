3e89d156fe74b8880150d85f8a2679033c2a5f8d
KYI <siderdax@naver.com>
2019-09-07 21:44:57 +0900

    First upload

---
+++ AesSample/DataEncryptor/AesMaze.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace AesSample.DataEncryptor
+{
+    public class AesMaze
+    {
+        public byte[] Key { get; private set; }
+        public byte[] Iv { get; private set; }
+
+        public void CreateKeyIv()
+        {
+            using(Aes iAes = Aes.Create())
+            {
+                Key = new byte[iAes.Key.Length];
+                Array.Copy(iAes.Key, Key, iAes.Key.Length);
+                Iv = new byte[iAes.IV.Length];
+                Array.Copy(iAes.IV, Iv, iAes.IV.Length);
+
+                ICryptoTransform ict = iAes.CreateEncryptor(Key, Iv);
+            }
+        }
+
+        public byte[] Encrypt(string data)
+        {
+            byte[] encryptedData;
+
+            if (data == null || data.Length <= 0)
+            {
+                throw new ArgumentNullException("data");
+            }
+
+            using(Aes iAes = Aes.Create())
+            {
+                iAes.Key = Key;
+                iAes.IV = Iv;
+
+                ICryptoTransform encryptor = iAes.CreateEncryptor(iAes.Key, iAes.IV);
+                using(MemoryStream ms = new MemoryStream())
+                {
+                    using(CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using(StreamWriter sw = new StreamWriter(cs))
+                        {
+                            sw.Write(data);
+                        }
+                        encryptedData = ms.ToArray();
+                    }
+                }
+            }
+
+            return encryptedData;
+        }
+
+        public string Decrypt(byte[] encryptedData)
+        {
+            if (encryptedData == null || encryptedData.Length <= 0)
+            {
+                throw new ArgumentNullException("encryptedData");
+            }
+
+            string decryptedData = null;
+
+            using(Aes iAes = Aes.Create())
+            {
+                iAes.Key = Key;
+                iAes.IV = Iv;
+
+                ICryptoTransform decryptor = iAes.CreateDecryptor(iAes.Key, iAes.IV);
+                using(MemoryStream ms = new MemoryStream(encryptedData))
+                {
+                    using(CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using(StreamReader srDecrypt = new StreamReader(cs))
+                        {
+                            decryptedData = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return decryptedData;
+        }
+
+        public AesMaze()
+        {
+            CreateKeyIv();
+        }
+
+        public AesMaze(byte[] key, byte[] iv)
+        {
+            Key = key;
+            Iv = iv;
+        }
+    }
+}
---
+++ AesSample/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using AesSample.DataEncryptor;
+
+namespace AesSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] customKey = new byte[]
+            {
+                16, 20, 98, 240, 43, 80, 238, 15, 189, 239, 63, 236, 170, 47, 106, 3, 44, 159, 50,
+                136, 226, 241, 109, 91, 94, 56, 94, 45, 45, 80, 37, 45
+            };
+            byte[] customIv = new byte[]
+            {
+                192, 193, 155, 175, 186, 68, 0, 37, 83, 243, 176, 170, 58, 126, 122, 20
+            };
+            string target = "This Text must be recovered.";
+            byte[] metamorphosis;
+
+            // AesMaze am = new AesMaze(); <- Create random Key, IV
+            AesMaze am = new AesMaze(customKey, customIv);
+
+            try
+            {
+                System.Console.WriteLine("Key:");
+                foreach (byte b in am.Key)
+                {
+                    System.Console.Write(b + " ");
+                }
+                System.Console.WriteLine("");
+                System.Console.WriteLine("IV:");
+                foreach (byte b in am.Iv)
+                {
+                    System.Console.Write(b + " ");
+                }
+                System.Console.WriteLine("");
+                System.Console.WriteLine("");
+
+                System.Console.WriteLine("Enctypting text(" + target + ")");
+                System.Console.WriteLine("result:");
+                metamorphosis = am.Encrypt(target);
+                foreach (byte b in metamorphosis)
+                {
+                    System.Console.Write(b + " ");
+                }
+                System.Console.WriteLine("");
+                System.Console.WriteLine("Decrypting");
+                System.Console.WriteLine("result:");
+                System.Console.WriteLine(am.Decrypt(metamorphosis));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
